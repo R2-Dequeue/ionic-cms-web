115e394d6133334154c692634f9bbc8668a2a848
Chris de Pujo <chris.depujo@gmail.com>
2017-08-10 12:00:16 -0400

    Start of recursive directive page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app';
+  items: number[] = [1, 2, 3, 4];
+  counter: number = 5;
+
+  push() {
+    this.items.push(this.counter);
+    this.counter += 1;
+  }
+
+  pop() {
+    this.items.pop();
+  }
+
+  document = {
+    title: 'A book',
+    items: [
+      {
+        title: 'Chapter 1',
+        items: [
+          {
+            title: 'Section 1',
+            items: []
+          },
+          {
+            title: 'Section 2',
+            items: []
+          },
+          {
+            title: 'Section 3',
+            items: []
+          }
+        ]
+      },
+      {
+        title: 'Chapter 2',
+        items: [
+          {
+            title: 'Section 1',
+            items: []
+          },
+          {
+            title: 'Section 2',
+            items: []
+          }
+        ]
+      },
+      {
+        title: 'Chapter 3',
+        items: [
+          {
+            title: 'Section 1',
+            items: []
+          }
+        ]
+      }
+    ]
+  };
 }
