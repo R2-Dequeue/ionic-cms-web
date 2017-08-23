c7472e879ec1eb455504cadeddf26a7d3118def8
Chris de Pujo <chris.depujo@gmail.com>
2017-08-23 16:30:22 -0400

    Adding Quill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+
+import * as Quill from 'quill';
 
 import { GuiService } from './gui.service';
 
@@ -10,8 +12,7 @@ import { GuiService } from './gui.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title: string = 'app';
+export class AppComponent implements AfterViewInit {
   documentRoot: any = { title: 'Loading...', items: [] };
   objectObservable: FirebaseObjectObservable<any>;
   documentsObservable: FirebaseListObservable<any>;
@@ -20,17 +21,48 @@ export class AppComponent {
     pageDisabled: true,
     submitPageDisabled: true
   };
+  pageEditor;
+  emptyArray = [];
 
   /**
    * @todo Add error handling to `.once` call.
    * @param db 
    * @param guiService 
    */
-  constructor(db: AngularFireDatabase, private guiService: GuiService) {
+  constructor(private db: AngularFireDatabase, private guiService: GuiService) {
     this.objectObservable = db.object('/pharmacopeia');
     this.objectObservable.$ref.once('value', snapshot => this.documentRoot = snapshot.val());
 
     guiService.setState(this.state);
+  }
+
+  ngAfterViewInit() {
+    // ql-toolbar, ql-container
+
+    let defaultModules = {
+      toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ 'color': this.emptyArray.slice() }, { 'background': this.emptyArray.slice() }],          // dropdown with defaults from theme
+        [{ 'font': this.emptyArray.slice() }],
+        [{ 'align': this.emptyArray.slice() }],
+
+        ['clean'],                                         // remove formatting button
+
+        ['link', 'image', 'video']                         // link and image, video
+      ]
+    };
+    this.pageEditor = new Quill('#quillPageEditor', { theme: 'snow' });
   }
 
   upload() {
